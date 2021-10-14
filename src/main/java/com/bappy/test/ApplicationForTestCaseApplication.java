539ad3497380d36a7ad9027b867af1c23a5a0c28
SohanBappy <sohanbappy@gmail.com>
2021-10-15 02:34:13 +0600

    Project initialized

---
+++ src/main/java/com/bappy/test/ApplicationForTestCaseApplication.java
@@ -0,0 +1,13 @@
+package com.bappy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationForTestCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationForTestCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bappy/test/ApplicationForTestCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bappy.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationForTestCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
