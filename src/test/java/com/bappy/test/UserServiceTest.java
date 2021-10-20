ec48d42ecbfd12edf946b115b3ae4b927e314aa7
SohanBappy <sohanbappy@gmail.com>
2021-10-21 02:21:15 +0600

    Service test added

---
+++ src/test/java/com/bappy/test/UserServiceTest.java
@@ -0,0 +1,35 @@
+package com.bappy.test;
+
+import com.bappy.test.entity.User;
+import com.bappy.test.repository.UserRepo;
+import com.bappy.test.service.UserService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+/*
+To enable to @Mock annotation we have to add the annotation @ExtendWith(MockitoExtension.class)
+
+ */
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    UserRepo userRepo;
+
+    @Test
+    @DisplayName("Get User By ID")
+    void getUserByIdTest() throws Exception {
+        User expectedUser = new User(500, "SB", "0166", "sb@gmail.com");
+        UserService service = new UserService(userRepo);
+        Mockito.when(userRepo.findById(500)).thenReturn(Optional.of(expectedUser));
+        User actualUser = service.getUserById(500);
+        Assertions.assertThat(actualUser.getId()).isEqualTo(expectedUser.getId());
+    }
+}
