3e8322ef1fb955e1d0be8ad1f2e9d5dce80b5727
SohanBappy <sohanbappy@gmail.com>
2021-10-21 10:06:42 +0600

    Secured single API call instead of All APIs of a method.

--- src/test/java/com/bappy/test/SecurityTest.java
+++ src/test/java/com/bappy/test/SecurityTest.java
@@ -1,10 +1,8 @@
 package com.bappy.test;
 
-import com.bappy.test.controller.HelloController;
 import com.bappy.test.controller.WebController;
 import com.bappy.test.entity.User;
 import com.bappy.test.service.UserService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,8 +10,8 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,7 +19,13 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Arrays;
 
+/*
+@WithMockUser(username = "sohan", roles = {"USER", "ADMIN"}) - It will be applied to ALL APIs of that method
 
+.with(SecurityMockMvcRequestPostProcessors.user("sohan").roles("ADMIN", "USER"))) - applied for Only that API call
+
+
+ */
 @WebMvcTest(controllers = WebController.class)
 public class SecurityTest {
 
@@ -32,7 +36,6 @@ public class SecurityTest {
 
     @Test
     @DisplayName("Return Index View (Protected)")
-    @WithMockUser(username = "sohan", roles = {"USER", "ADMIN"})
     public void getAllUsersTest() throws Exception {
         User user1 = new User(101, "sohan", "01766", "sohan@gmail.com");
         User user2 = new User(102, "bappy", "01866", "bappy@gmail.com");
@@ -40,7 +43,8 @@ public class SecurityTest {
         Mockito.when(userService.getAllUsers()).thenReturn(Arrays.asList(user1, user2));
 
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/index"))
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/index")
+                .with(SecurityMockMvcRequestPostProcessors.user("sohan").roles("ADMIN", "USER")))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("index"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("msg"))
