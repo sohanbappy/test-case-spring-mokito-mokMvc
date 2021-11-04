3a9ab544ee9c810bccf3b66bc3cd8d877fa4ce6c
SohanBappy <sohanbappy@gmail.com>
2021-11-05 01:53:03 +0600

    Private-Method testing using Reflections

--- src/test/java/com/bappy/test/StaticPrivateMethodTest.java
+++ src/test/java/com/bappy/test/StaticPrivateMethodTest.java
@@ -3,18 +3,51 @@ package com.bappy.test;
 
 import com.bappy.test.helper.Converter;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.powermock.reflect.Whitebox;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class StaticPrivateMethodTest {
 
-    @Test
-    public void privateMethodTest() throws Exception {
-        Converter converter = new Converter();
+    @Mock
+    Converter converter;
 
-        String actualResult = Whitebox.invokeMethod(converter, "getResult", 70);
-        String expectedResult = "Pass";
-        Assertions.assertThat(expectedResult).isEqualTo(actualResult);
+    @BeforeEach
+    public void createObject() {
+        converter = new Converter();
     }
 
+
+    @Test
+    public void privateMethodTest() throws Exception {
+        String actualResult = Whitebox.invokeMethod(converter, "getResult", 70);
+        String expectedResult = "Pass";
+
+        Assertions.assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void privateMethodTestUsingReflectionUtils() {
+        ReflectionTestUtils.invokeMethod(converter, "getResult", 50);
+    }
+
+    @Test
+    public void privateMethodTestUsingReflection() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class[] params = new Class[1];//no. of parameters
+        params[0] = int.class;//Don't Use Integer, plz
+        Method method = converter.getClass().getDeclaredMethod("getResult", params);
+        method.setAccessible(true);
+
+        Object[] methodArgs = new Object[1];
+        methodArgs[0] = 20;//marks
+        String actual = (String) method.invoke(converter, methodArgs);
+        String expected = "Fail";
+
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
 }
